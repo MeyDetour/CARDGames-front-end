0e1c04e80043d1197075eab355adfb4dee8d37b3
Mey <ton.email@exemple.com>
2026-08-17 10:00:22 +0200

    Change url of divers app (fixe#9)

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 export const env = {
-  CARD_STUDIO_FRONT_END_URL: "http://localhost:5173",   
-  CARD_STUDIO_API_URL: "https://card-studio-api.genos-center.com/",  
-  CARD_STUDIO_WEBSOCKET_URL: "ws://localhost:8008"
+  CARD_STUDIO_FRONT_END_URL: "https://card-studio.genos-center.com",   
+  CARD_STUDIO_API_URL: "card-studio-api.genos-center.com",  
+  CARD_STUDIO_WEBSOCKET_URL: "ws://card-maker-server.genos-center.com:8080"
 };
  
