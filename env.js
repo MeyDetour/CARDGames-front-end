8690063de5dc16f4140bba199905ecb61136ac6e
Mey <ton.email@exemple.com>
2026-08-20 14:47:35 +0200

    add slash (fixe#9)

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 export const env = {
   CARD_STUDIO_FRONT_END_URL: "https://card-studio.genos-center.com",   
-  CARD_STUDIO_API_URL: "https://card-studio-api.genos-center.com",  
+  CARD_STUDIO_API_URL: "https://card-studio-api.genos-center.com/",  
   CARD_STUDIO_WEBSOCKET_URL: "ws://card-maker-server.genos-center.com:8080"
 };
  
